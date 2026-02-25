42a0ee7fb2a2b9993668cf6c9acb561bd6ab2fc1
Mai1sha <maishaislam200051@gmail.com>
2026-02-25 15:55:57 -0600

    added initial backend files

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app= express()
+
+app.get('/', (req, res) =>  {
+    res.send('Hello from express')
+})
+
+app.get('/about', (req, res) =>{
+    res.send('Hello from about page')
+})
+
+app.listen(3000, () => {
+    console.log('The server is running')
+})c
